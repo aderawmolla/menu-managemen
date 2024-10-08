6da1119411760f3c883a716b4b1df27aeaf2adc7
aderawmolla <mollaaderaw@gmail.com>
2024-10-08 17:42:20 +0300

    deploy

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:2000/api',  // Backend base URL
+  baseURL: 'https://menu-managemen-1.onrender.com/api',  // Backend base URL
 });
 
 export const fetchMenus = () => api.get('/menus');
